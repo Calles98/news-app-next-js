9a3d968f24d79f6826d348424d0071d871259fe9
Rodrigo Calles <joserodrigoc98@gmail.com>
2023-11-08 09:52:49 -0700

    navlinks

--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -14,7 +14,7 @@ const NavLinks = () => {
     };
 
     return (
-        <nav className='grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm gap-4 pb-10 max-w-6xl mx-auto border-b'>
+        <nav className='grid grid-cols-2 md:grid-cols-7 text-xs md:text-sm gap-4 pb-10 max-w-6xl mx-auto border-b'>
             {categories.map((category) => (
                 <NavLink key={category} category={category} isActive={isActive(category)} />
             ))}
